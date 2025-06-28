7d4eacfab2090d1556d2bfdfcbca795f4507dfb1
prasanjit-paul <prasanjitpaul2003@gmail.com>
2025-06-28 18:34:38 +0530

    Router done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom' 
+import Home from "./router/Home"
+import Blog from "./router/Blog"
+import Career from "./router/Career"
+import Community from "./router/Community"
+import Profile from "./router/Profile"
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/community">Community</Link>
+        <Link to="/career">Career</Link>
+        <Link to="/profile">Profile</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
     </>
   )
 }
